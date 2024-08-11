5e5670f14e40912dc443e2e6da49444bc088bed7
Omkar Raykar <raykaromkar1244@gmail.com>
2024-08-11 21:47:55 +0530

    added new file conversionOperation

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -3,19 +3,20 @@
 // let score = undefined
 let score = true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber); 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
 // 1. 33abc => 33
 // 2. 33abc => NaN
 // 3. true => 1; false => 0
 
 let isLoggedIn = true // try putting different value
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -24,5 +25,33 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let str1 = 2
+let str2 = 3
+let str3 = str1 + str2 // add
+// let str3 = str1 - str2 // substract
+// let str3 = str1 * str2 // miltiply
+// let str3 = str1 ** str2 // to the power
+// let str3 = str1 / str2 // divide
+// let str3 = str1 % str2 // modulo
+
+// console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+// console.log(+true); //true
+// console.log(+""); //false
+
+let gameCounter = 100
+gameCounter++; //postfix
+++gameCounter; //prefix
+// console.log(gameCounter);
+
+
+
+
+
