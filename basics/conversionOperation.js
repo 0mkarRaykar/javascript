8ae4c8d06f1a3f4c8868ce8a547c7c8ffeb429ce
Omkar Raykar <raykaromkar1244@gmail.com>
2024-08-11 21:07:32 +0530

    added new file conversionOperation

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,11 +1,18 @@
 // let score = "33abc"
-let score = null
+// let score = null
 // let score = undefined
+let score = true
 
 console.log(typeof score);
 console.log(typeof (score));
 
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
-console.log(valueInNumber); // 1. dataType is NaN => Not a Number" if input is 33abc or undefined
-// 2. dataType is null => if input is null
+console.log(valueInNumber); 
+// 1. 33abc => 33
+// 2. 33abc => NaN
+// 3. true => 1; false => 0
+
+let isLoggedIn = true // try putting different value
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
