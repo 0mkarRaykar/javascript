b5e6b5dfb408be2963a2734554709cf0b54f26be
Omkar Raykar <raykaromkar1244@gmail.com>
2024-08-11 20:41:00 +0530

    added new dataTypes file

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -4,7 +4,8 @@ let isLoggedIn = false // boolean => true or false; value either be true or fals
 let state; // undefined; state is initialised but not asigned any value
 // null => standalone value
 // symbol => unique 
+// bigint => when number will be bigger than 2 to the power 53
 
 // object
-console.log(typeof undefined);
-console.log(typeof null);
+console.log(typeof undefined); // dataType is undefined
+console.log(typeof null); // dataType is object
