970d281260e53bdf671049efb904b7d21d2e576e
Omkar Raykar <raykaromkar1244@gmail.com>
2024-08-11 20:39:21 +0530

    added new dataTypes file

---
+++ basics/dataTypes.js
@@ -0,0 +1,10 @@
+let name = "omkar" //String
+let age = 18 // number
+let isLoggedIn = false // boolean => true or false; value either be true or false
+let state; // undefined; state is initialised but not asigned any value
+// null => standalone value
+// symbol => unique 
+
+// object
+console.log(typeof undefined);
+console.log(typeof null);
