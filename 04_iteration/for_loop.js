1c539f66dd89260bbb02945f48067bf786aa1d25
Omkar Raykar <raykaromkar1244@gmail.com>
2024-08-16 20:23:20 +0530

    modified for_loop file

--- 04_iteration/for_loop.js
+++ 04_iteration/for_loop.js
@@ -2,15 +2,31 @@
 
 for (let index = 1; index <= 10; index++) {
     const element = index;
-    console.log(element);
+    // console.log(element);
 }
 
 // nested for loop
 
-// for (let i = 1; i <= 10; i++) {
-//     console.log(`Outer loop value: ${i}`);
-//     for (let j = 1; j <= 10; j++) {
-//         console.log(`Inner loop value: ${j} and inner loop ${i}`);
-//     }
-// }
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner loop value: ${j} and inner loop ${i}`);
+    }
+}
 
+// break and continue in for loop
+for (let i = 0; i <= 10; i++) {
+    if (i == 5) {
+        // console.log(`Detected 5`);
+        break; // loop will end
+    }
+    // console.log(i);
+}
+
+for (let i = 0; i <= 10; i++) {
+    if (i == 5) {
+        // console.log(`Detected 5`);
+        continue; // loop will skip this condition and will continue execution
+    }
+    // console.log(i);
+}
