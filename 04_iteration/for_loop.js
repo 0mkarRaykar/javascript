24a013a9aec9bf6e6c1648463ae93e690e53e00e
Omkar Raykar <raykaromkar1244@gmail.com>
2024-08-16 20:12:08 +0530

    added new folder and add some files too

---
+++ 04_iteration/for_loop.js
@@ -0,0 +1,16 @@
+// for loop syntax
+
+for (let index = 1; index <= 10; index++) {
+    const element = index;
+    console.log(element);
+}
+
+// nested for loop
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`Inner loop value: ${j} and inner loop ${i}`);
+//     }
+// }
+
