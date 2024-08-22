fd30877e57f2921cafc173f916225962ba78de5c
Omkar Raykar <raykaromkar1244@gmail.com>
2024-08-22 17:48:59 +0530

    modified dir

--- learn-js/08_advance_one/fetch.js
+++
@@ -1,7 +0,0 @@
-const Url = 'https://api.github.com/users/0mkarraykar'
-
-fetch(Url).then((response)=>{
-    return response.json()
-}).then((data)=>{
-    console.log(data);
-}).catch((error)=> console.log(error))
